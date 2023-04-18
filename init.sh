750ff31a175e0d5398713aadb71dd2d3972c0591
Varun Gandhi <varun.gandhi@sourcegraph.com>
2023-04-18 14:58:19 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 sudo apt-get update -y
 sudo apt-get install -y gpg wget curl unzip git
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
-echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ $(lsb_release --codename --short) main' | sudo tee /etc/apt/sources.list.d/kitware.list >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ $(lsb_release --codename --short) main" | sudo tee /etc/apt/sources.list.d/kitware.list >/dev/null
 sudo rm /usr/share/keyrings/kitware-archive-keyring.gpg
 sudo apt-get update -y
 sudo apt-get install -y zsh kitware-archive-keyring cmake g++ ninja-build
